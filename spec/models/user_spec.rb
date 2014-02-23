568cb514b7e7326803e85946b40eea5cc84a61f9
Prashant Dang <prashantdang@gmail.com>
2014-02-23 12:13:32 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -22,9 +24,9 @@ describe User do
 	before do 
 		@user = User.new(name: "Exmaple User", email: "user@example.com",
 						password: " ", password_confirmation: " ")
-	end 
-	it { should_not be_valid }
-end
+		end 
+		it { should_not be_valid }
+	end
 
 	describe "when password doesn't match confirmation" do
 		before { @user.password_confirmation = "mismatch" }
@@ -108,6 +110,11 @@ end
 	describe "with a password that's too short" do
 	  before { @user.password = @user.password_confirmation = "a" * 5 }
 	  it { should be_invalid }
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 
 end
